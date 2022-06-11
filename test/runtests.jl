9db24b86b80f02fdd73e34773be234d71fcfd4c9
Mohamed Tarek <mohamed82008@gmail.com>
2022-06-12 01:43:14 +0400

    NonconvexIpopt test dep

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using NonconvexUtils, ForwardDiff, ReverseDiff, Tracker, Zygote
 using Test, LinearAlgebra, SparseArrays, NLsolve, IterativeSolvers
-using StableRNGs, ChainRulesCore, NonconvexCore
+using StableRNGs, ChainRulesCore, NonconvexCore, NonconvexIpopt
 
 include("forwarddiff_frule.jl")
 include("abstractdiff.jl")
