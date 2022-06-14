b05e60453d008cda9f8a227e71795567a5773459
Mohamed Tarek <mohamed82008@gmail.com>
2022-06-15 00:08:58 +0400

    test sparsity

--- test/sparse_forwarddiff.jl
+++ test/sparse_forwarddiff.jl
@@ -7,15 +7,23 @@
         f = sparsify(x -> 2(x.^2) + x[1] * ones(3), rand(3); hessian = false)
         x = rand(3)
         @test Zygote.jacobian(f, x)[1] ≈ ForwardDiff.jacobian(f, x)
+        @test NonconvexCore.sparse_jacobian(f, x) ≈ Zygote.jacobian(f, x)[1]
+        @test NonconvexCore.sparse_fd_jacobian(f, x) ≈ ForwardDiff.jacobian(f, x)
+        @test NonconvexCore.sparse_jacobian(f, x) isa SparseMatrixCSC
+        @test NonconvexCore.sparse_fd_jacobian(f, x) isa SparseMatrixCSC
 
         f = sparsify(sum, rand(3); hessian = true)
         x = rand(3)
         @test Zygote.gradient(f, x)[1] ≈ ForwardDiff.gradient(f, rand(3))
         @test Zygote.hessian(f, x) ≈ ForwardDiff.hessian(f, rand(3))
+        @test NonconvexCore.sparse_hessian(f, x) ≈ Zygote.hessian(f, x)
+        @test NonconvexCore.sparse_hessian(f, x) isa SparseMatrixCSC
 
         f = sparsify(x -> sum(x)^2 + x[1], rand(3); hessian = true)
         x = rand(3)
         @test Zygote.hessian(f, x) ≈ ForwardDiff.hessian(f, x)
+        @test NonconvexCore.sparse_hessian(f, x) ≈ Zygote.hessian(f, x)
+        @test NonconvexCore.sparse_hessian(f, x) isa SparseMatrixCSC
 
         f = sparsify(x -> sum(x)^2 + x[1], rand(3); hessian = true)
         x = rand(3)
